068a1e07c0731ef1728b0dce0a5b2a22f40c6ded
ndpease <58533664+ndpease@users.noreply.github.com>
2019-12-04 17:28:39 -0500

    Add files via upload

---
+++ GroupProject_Github/RoomService.java
@@ -0,0 +1,43 @@
+package GroupProject_v2_01;
+
+
+public class RoomService {
+    public int roomServID = 0;
+    
+    public String roomServDescription;
+    public double roomServPrice; 
+    
+    public static int nextID = 0; 
+    
+    
+    public RoomService(String roomServDescription, double roomServPrice)
+    {
+        this.roomServDescription = roomServDescription;
+        this.roomServPrice = roomServPrice;
+        this.roomServID = nextID++; 
+    }
+    
+}
+
+//// Add to booking class
+/*
+// RoomService ArrayList
+
+    public ArrayList<RoomService> roomServices;
+
+
+
+
+/// OrderRoomService() method 
+    public void orderRoomService(String roomServDesc, double roomServPrice)
+    {
+        this.roomServices.add(new RoomService(roomServDesc, roomServPrice));
+       
+    }
+
+// ShowRoomServices for a booking
+    public void showRoomService()
+    {
+        roomServices.forEach(value -> System.out.println(value));
+    }
+*/
