4ee4f1bde2edd0b24371f1b8950a79d88446eb04
ConnorHotaling <58533635+ConnorHotaling@users.noreply.github.com>
2019-12-07 19:04:14 -0500

    Add files via upload

--- DB_Application_Code.java
+++ DB_Application_Code.java
@@ -1,16 +1,82 @@
 package oracledb;
 import java.sql.*;
 import oracle.jdbc.pool.*;
+import java.util.*;
 
 public class DBTest {
-	public static void main(String[] args) {
+	
+	// IN THE PUBLIC CLASS METHOD BEFORE START()
+	Connection dbConn;
+	Statment commStmt;
+	ResultSet dbResults;
+	
+	String empTbl = "EMPLOYEE";
+	String guesTbl = "GUEST";
+	String bookingTbl = "BOOKING";
+	String roomTbl = "ROOM";
+	String valTbl = "VALUE_GUEST";
+	String roomServTbl = "ROOM_SERVICE";
+	
+	// NEED TO ADD FOR LOOPS TO EACH OF THE INSERT METHODS!!!!!!!!!!!!!!!!!!!!!!
+	
+	public void insertEmployee() {
+		String sqlQuery = "INSERT INTO JAVAUSER." + empTbl + " (EMPLOYEEID,NAME,USERNAME,PASSWORD) VALUES (";
+		sqlQuery += "\'" + Employee.employees.getEmployeeID().getText() + "\'"; // might not need the .getText()
+		sqlQuery += txtEmpName.getText() + ",";
+		sqlQuery += txtEmpUsername.getText() + ",";
+		sqlQuery += txtEmpPassword.getText() + ");";
 		
+		sendDBCommand(sqlQuery)
+	}
+	
+	public void insertGuest() {
+		String sqlQuery = "INSERT INTO JAVAUSER." + guesTbl + " (GUESTID,NAME,USERNAME,PASSWORD) VALUES (";
+		sqlQuery += "\'" + Guest.guests.getGuestID().getText() + "\'";
+		sqlQuery += txtGuestName.getText() + ",";
+		sqlQuery += txtGuestUser.getText() + ",";
+		sqlQuery += txtGuestPass.getText() + ");"
+		
+		sendDBCommand(sqlQuery);
+	}
+	
+	public void insertRoom() {
+		String sqlQuery = "INSERT INTO JAVAUSER." + roomTbl + " (ROOMID,ROOMNUMBER,ROOMBOOKEDQUANTITY,
+			ROOMBOOKED,BEDOPTION,KITCHENOPTION,COFFEEOPTION,ACCESSIBLEOPTION,ROOMCOSTPERNIGHT,ROOMSTATUS) VALUES (";
+		sqlQuery += // roomID
+		sqlQuery += tfERRoomNo.getText() + ",";
+		sqlQuery += // Room quantity + ",";
+		sqlQuery += // Room booked
+		sqlQuery += cbRoomBedOpt.getText() + ",";
+		sqlQuery += cbRoomKitchOpt.getText() + ",";
+		sqlQuery += cbRoomCoffeeOpt.getText() + ",";
+		sqlQuery += cbRoomAccessOpt.getText() + ",";
+		
+		
+		sendDBCommand(sqlQuery);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+		
+	public void sendDBCommand(String sqlQuery) {		
 		String jdbcConnectionURL = "jdbc:oracle:thin:@localhost:1521:XE";
 		String userID = "javauser";
 		String userPASS = "javapass";
+		OracleDataSource ds;
 		
-		Statement stmt;
-		ResultSet rset;
-		String sqlQuery;
+		System.out.println(sqlQuery);
 		
-		
+		try {
+			ds = new OracleDataSource();
+			ds.setURL(URL);
+			dbConn = ds.getConnection(userID,userPASS);
+			commStmt = dbConn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			dbResults = commStmt.executeQuery(sqlQuery);
+		}
+		catch (SQLException e) {
+			System.out.println(e.toString());
+		}
+	}	
